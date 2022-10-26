4faa762c4cde2d87bd07160ca6d90976eaa767fe
Joe Watkins <watkinsjoe99@live.com>
2022-10-25 19:53:32 -0500

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React, { ChangeEvent, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import ScoreTable from "./components/ScoreTable";
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
 function App() {
-  return (
-    <>
-      <ScoreTable />
-    </>
-  );
+  return <ScoreTable />;
 }
 
 export default App;
